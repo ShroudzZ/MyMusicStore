0419f4feab1f9369954263f22c67c8dfbb0e8d8d
ShroudzZ <1833549764@qq.com>
2018-12-02 10:15:38 +0800

    12.2

---
+++ Shroud/MusicStore/MusicStore/MusicStore/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStoreEntity;
+
+namespace MusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly MusicContext _context = new MusicContext();
+        // GET: test
+        public ActionResult Index()
+        {
+            var list = _context.Albums.OrderByDescending(x=>x.PublisherDate).Take(20).ToList();
+            return View(list);
+        }
+
+        public ActionResult Genre(Guid id)
+        {
+            var list = _context.Albums.Where(g=>g.Genre.ID==id).OrderByDescending(x => x.PublisherDate).ToList();
+            return View(list);
+        }
+    }
+}
---
+++ Shroud/MusicStore/MusicStore/MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStoreEntity;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        private static readonly MusicContext _context = new MusicContext();
+        // GET: Store
+        public ActionResult Index()
+        {
+            var list = _context.Genres.ToList();
+            return View(list);
+        }
+
+        public ActionResult Detail(Guid id)
+        {
+	    var album = _context.Albums.Find(id);
+
+            return View(album);
+           
+        }
+    }
+}
